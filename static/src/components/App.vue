135b3e834fdf4bd7cd4d46594e3f6ec08b9b1eb5
j-haines <me@jhain.es>
2020-04-14 05:46:03 +0000

    initial commit

---
+++ static/src/components/App.vue
@@ -0,0 +1,22 @@
+<template>
+	<div @click="hello">Hello, {{ name }}!</div>
+</template>
+
+<script lang="ts">
+import {Vue, Component} from "vue-property-decorator"
+
+@Component
+export default class App extends Vue {
+	// undecorated property will be packed in `data` option
+	name = "world"
+
+	// undecorated method will be packed in `methods` option
+	hello() {
+		alert("Hello, " + this.name)
+	}
+}
+</script>
+
+<style>
+
+</style>
